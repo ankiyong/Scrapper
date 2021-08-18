d9a10591ee27c75482d7d8500afa66a91f81c67e
ankiyong <85871256+ankiyong@users.noreply.github.com>
2021-08-18 20:04:04 +0000

    finally i made it

--- main.py
+++ main.py
@@ -1,44 +1,45 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from scrapper import get_jobs
-
+from exporter import save_to_file
 app = Flask("SuperScrapper")
 
 db = {}
 
-# @는 바로 아래 있는 함수를 찾게된다
-# 기본 주소에 괄호안의 문자를 입력하면 @이 바로 아래 있는 함수를 찾아서
-# 해당 기능을 동작하게 되는 원리
+
 @app.route("/")
 def home():
     return render_template("potato.html")
 
 
-@app.route("/report")
+@app.route('/report')
 def report():
     word = request.args.get('word')
     if word:
         word = word.lower()
         existingJobs = db.get(word)
         if existingJobs:
-          jobs = existingJobs
+            jobs = existingJobs
         else:
-          jobs = get_jobs(word)
-          db[word] = jobs
+            jobs = get_jobs(word)
+            db[word] = jobs
     else:
         return redirect('/')
-    return render_template('report.html',        searchingBy=word,
-      resultsNumber=len(jobs),
-      jobs=jobs
-    )
-
-
-# /contact 로 접속 요청이 들어오면
-# 파이썬이 아~ 바로 아래있는 함수 contact를 실행하면 되겠구나~
-# 라고 생각하는것임
-# @app.route("/<username>")
-# def contact(username):
-#   return
-
+    return render_template("report.html",word=word, resultsNumber=len(jobs),jobs=jobs)
+@app.route('/export')
+def export():
+  try:
+    word = request.args.get('word')
+    if not word:
+      raise Exception()
+    word = word.lower()
+    jobs = db.get(word)
+    if not jobs:
+      raise Exception()
+    save_to_file(jobs)
+    return send_file("jobs.csv")
+  except:
+    return redirect("/")
   
+
 
 app.run(host="0.0.0.0")
--- scrapper.py
+++ scrapper.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 
+
+
 def get_last_page(url):
     result = requests.get(url)
     soup = BeautifulSoup(result.text, 'html.parser')
@@ -27,11 +29,11 @@ def extract_job(html):
     }
 
 
-def extract_jobs(last_page, url):
+def extract_jobs(last_page,url):
     jobs = []
     for page in range(last_page):
         print(f"Scrapping SO : Page {page}")
-        result = requests.get(f'{url}&pg={page + 1}')
+        result = requests.get(f'{url}&pg={page+1}')
         soup = BeautifulSoup(result.text, 'html.parser')
         results = soup.find_all('div', {'class': '-job'})
         for result in results:
@@ -43,6 +45,6 @@ def extract_jobs(last_page, url):
 def get_jobs(word):
     url = f'https://stackoverflow.com/jobs?q={word}'
     last_page = get_last_page(url)
-    jobs = extract_jobs(last_page, url)
-
+    jobs = extract_jobs(last_page,url)
+    
     return jobs
